139be9302158be75cd88e30908027536702ac6f4
David Foot <david.foot@outlook.be>
2024-06-27 17:28:28 +0200

    prout2

--- Assets/Scripts/cardClicked.cs
+++ Assets/Scripts/cardClicked.cs
@@ -1,12 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityAtoms.BaseAtoms;
 using UnityEngine;
 
 public class cardClicked : MonoBehaviour
 {
     // Start is called before the first frame update
+    [SerializeField] Color selectedColor;
+    [SerializeField] GameObject sprite;
+    [SerializeField] IntReference maxDiscardCard;
+    [SerializeField] IntReference currentSelectedCard;
     public void CardClicked()
     {
-        Debug.Log("CardClicked");
+        if (currentSelectedCard.Value < maxDiscardCard.Value) {
+            sprite.GetComponent<SpriteRenderer>().material.color = selectedColor;
+            currentSelectedCard.Value++;
+        }
     }
 }
---
+++ Assets/Scripts/cardSpriteClick.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityAtoms.BaseAtoms;
+using UnityEngine;
+
+public class cardSpriteClick : MonoBehaviour
+{
+    [SerializeField] BoolEventInstancer cardClicked;
+    [SerializeField] BoolReference isSelected;
+
+    private void OnMouseDown()
+    {
+        
+        cardClicked.Event.Raise(!isSelected);
+    }
+}
